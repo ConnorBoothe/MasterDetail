57eebc00038ea77a124ad5afd74988119e898980
Connor Boothe <cboothe@ccgiq.com>
2023-06-28 13:58:44 -0400

    Initial Commit

---
+++ MasterDetail/MasterDetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterDetailApp.swift
+//  MasterDetail
+//
+//  Created by Connor Boothe on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MasterDetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
